54502b5715a998b6703c4fcb4e3fc94a97931d89
Andrey Pudov <mail@andreypudov.com>
2021-07-07 11:35:49 +0300

    ~ initial commit

---
+++ FormattingSample/Program.cs
@@ -0,0 +1,23 @@
+﻿// <copyright file="Program.cs" company="Andrey Pudov">
+//     Copyright (c) Andrey Pudov. All Rights Reserved. Licensed under the Apache License, Version 2.0. See LICENSE.txt in the project root for license information.
+// </copyright>
+
+namespace FormattingSample
+{
+    using System;
+
+/// <summary>
+    /// The command-line interface for FormattingSample.
+    /// </summary>
+    internal static class Program
+    {
+        /// <summary>
+        /// The entry point of the application.
+        /// </summary>
+        /// <param name="args">The list of command-line parameters.</param>
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
